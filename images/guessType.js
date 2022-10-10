c0410eaa0e7070e7cad08109cf6e9f68b00156da
Jan Miksovsky <jan@miksovsky.com>
2022-10-10 16:16:16 -0700

    flowstate images

--- images/guessType.js
+++ images/guessType.js
@@ -1,7 +1,11 @@
 import sharp from "sharp";
 
 export default async function guessType(buffer) {
-  const image = sharp(buffer);
-  const metadata = await image.metadata();
-  return metadata.format;
+  try {
+    const image = sharp(buffer);
+    const metadata = await image.metadata();
+    return metadata.format;
+  } catch (e) {
+    return "unknown";
+  }
 }
