7724094e2f6e68ab7bd75ddee426c8e6f94205ff
Jan Miksovsky <jan@miksovsky.com>
2022-10-11 16:17:43 -0700

    Group by year, leave only month-date in name

--- sandbox/groupByYear.js
+++ sandbox/groupByYear.js
@@ -1,14 +1,15 @@
 export default function groupByMonth(posts) {
   const result = {};
   for (const key of Object.keys(posts)) {
-    const regex = /^(\d{4})/;
+    const regex = /^(\d{4})-(.+)$/;
     const match = key.match(regex);
     if (match) {
       const year = match[1];
+      const rest = match[2];
       if (!result[year]) {
         result[year] = {};
       }
-      result[year][key] = posts[key];
+      result[year][rest] = posts[key];
     } else {
       console.error(`Could not parse date from ${key}`);
     }
