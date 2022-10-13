544c39db7fe4cc681a4fd18800f63fd568fe5f64
Jan Miksovsky <jan@miksovsky.com>
2022-10-13 16:03:53 -0700

    Add feed.json and RSS

---
+++ src/feedItem.js
@@ -0,0 +1,23 @@
+import { ExplorableGraph } from "@graphorigami/origami";
+
+export default async function feedItem(buffer) {
+  const text = String(buffer);
+  const plain = await ExplorableGraph.plain(text);
+  const { date, slug, title } = plain;
+  const content_html = plain["@text"];
+
+  // Date will not have time zone; shift to Pacific time.
+  const date_published = new Date(`${date} GMT-7`);
+
+  const year = date_published.getFullYear();
+  const key = await this.get("@key");
+  const url = `https://jan.miksovsky.com/posts/${year}/${key}`;
+
+  return {
+    content_html,
+    date_published,
+    id: url,
+    url,
+    title,
+  };
+}
