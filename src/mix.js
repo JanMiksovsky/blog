b767e15bd83833d37db4240009901d8bc159b3a4
Jan Miksovsky <jan@miksovsky.com>
2022-12-07 16:45:08 -0800

    Update origami

--- src/mix.js
+++
@@ -1,8 +0,0 @@
-import { MetaTransform, ObjectGraph } from "@graphorigami/origami";
-
-export default function mix(...graphs) {
-  const result = new (MetaTransform(ObjectGraph))({});
-  result.parent = this;
-  result.peerAdditions = graphs;
-  return result;
-}
