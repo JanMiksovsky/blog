a7fc71669686e55b325ae324793bd852130363df
Jan Miksovsky <jan@miksovsky.com>
2022-12-24 11:21:14 -0800

    Successful post of media to Mastodon via Node without a framework

--- sandbox/postMedia.js
+++ sandbox/postMedia.js
@@ -1,65 +1,23 @@
-// import fs from "fs";
-import { login } from "masto";
-// import { Readable } from "stream";
-
-const masto = await login({
-  url: "https://fosstodon.org",
-  accessToken: "QYDjQOfn25wNWCNbdEIYl5s6vqFQ4--IIq42KRua8-s",
-});
-
-// Desired ability to post media via REST API seems blocked by
-// https://github.com/mastodon/mastodon/issues/17622
 //
-// HACK: Use Masto.js to post media as a workaround, which only seems to work
-// with actual files on disk.
+// Desired ability to post media to Mastodon via REST API seems to be impaired
+// by a Node issue:
+// https://github.com/mastodon/mastodon/issues/17622#issuecomment-1364535599.
+//
+// Per that comment, we take care to: a) convert the Buffer to a Blob b) *not*
+// set a file name c) *not* set the Content-Type header
+//
 export default async function postMedia(media, description) {
-  fs.writeFileSync("temp.png", media);
-  const stream = fs.createReadStream("temp.png");
-  const attachment = await masto.mediaAttachments.create({
-    file: stream,
-    description,
-  });
-  fs.unlinkSync("temp.png");
-  return attachment;
-
-  const base64 = media.toString("base64");
-  // const stream = Readable.from(base64);
-
-  // const blob = new Blob([media], { type: "image/png" });
-  // const file = new File([blob], "test.png", { type: "image/png" });
-  // const formData = new FormData();
-  // formData.append("description", description);
-  // // formData.append("file", blob, "test.png");
-  // formData.append("file", media, {
-  //   contentType: "image/png",
-  //   filename: "test.png",
-  // });
-  // formData.append("file", file);
-  // const stream = fs.createReadStream("test.png");
-  // formData.append("file", stream, "temp.png");
-
-  // const image = fs.createReadStream("test.png");
-  // formData.append("file", image);
-
-  // const istream = Buffer.from(media, "base64");
-  // formData.append("file", istream);
-
-  const body = `
---boundary
-Content-Disposition: form-data; name="file"; filename="test.png"
-
-${base64}
---boundary--
-`;
+  const blob = new Blob([media]);
+  const formData = new FormData();
+  formData.append("description", description);
+  formData.append("file", blob);
 
   const response = await fetch("https://fosstodon.org/api/v2/media", {
     method: "POST",
     headers: {
       Authorization: "Bearer QYDjQOfn25wNWCNbdEIYl5s6vqFQ4--IIq42KRua8-s",
-      "Content-Type": `multipart/form-data;boundary="boundary"`,
     },
-    // body: formData,
-    body,
+    body: formData,
   });
 
   if (response.ok) {
