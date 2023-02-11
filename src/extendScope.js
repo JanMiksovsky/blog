593229c477ea9154dca32e4c92e561c5742d8d80
Jan Miksovsky <jan@miksovsky.com>
2023-02-11 11:09:55 -0800

    Promote extendScope()

--- src/extendScope.js
+++
@@ -1,6 +0,0 @@
-import { setScope } from "@graphorigami/origami";
-
-export default function extendScope(graph, ...scopeGraphs) {
-  const scope = this;
-  return setScope.call(scope, graph, ...scopeGraphs, scope);
-}
