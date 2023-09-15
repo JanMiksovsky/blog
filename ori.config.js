cf8d0493edf235ca5b78c88b5418c3771ce2aaa7
Jan Miksovsky <jan@miksovsky.com>
2023-09-15 15:48:15 -0700

    Restore ori config

--- ori.config.js
+++ ori.config.js
@@ -1,15 +1,18 @@
 import {
-  builtins,
+  CommandModulesTransform,
   FilesGraph,
-  ImplicitModulesTransform,
+  ImportModulesMixin,
   MergeGraph,
+  builtins,
 } from "@graphorigami/origami";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const toolsPath = path.resolve(dirname, "tools");
-const tools = new (ImplicitModulesTransform(FilesGraph))(toolsPath);
+const tools = new (CommandModulesTransform(ImportModulesMixin(FilesGraph)))(
+  toolsPath
+);
 
 const config = new MergeGraph(builtins, tools);
 export default config;
