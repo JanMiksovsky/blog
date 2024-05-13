8f0b173f6da7830b3c6057e92039234f447e1415
Jan Miksovsky <jan@miksovsky.com>
2024-05-13 11:16:01 -0700

    Track changes, clean up

--- src/postYear.js
+++
@@ -1,8 +0,0 @@
-export default async function postYear(dateText) {
-  if (!dateText) {
-    return undefined;
-  }
-  const date = new Date(`${dateText} PST`);
-  const year = date.getFullYear();
-  return year;
-}
